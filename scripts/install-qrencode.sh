9e53e4676e518f3e4c8c09389a87bac597cf700b
Igor Chubin <igor.sl@chub.in>
2018-12-20 14:33:08 +0300

    added scripts/install-qrencode.sh

---
+++ scripts/install-qrencode.sh
@@ -0,0 +1,15 @@
+sudo apt-get update -y
+sudo apt-get install -y nginx git-core qrencode python-virtualenv
+
+sudo cp config/qrencode.conf /etc/nginx/sites-available/default
+
+git clone https://github.com/chubin/qrenco.de
+cd qrenco.de
+virtualenv ve/
+ve/bin/pip install -r requirements.txt 
+
+mkdir -p log/
+nohup ve/bin/python bin/srv.py >> log/qrencode.log 2>&1 &
+
+sudo /etc/init.d/nginx restart
+
