cca1a84d69873907e8685d359232f60f2ad59087
Igor Chubin <igor.sl@chub.in>
2018-12-20 14:33:47 +0300

    added start_vm()

--- scripts/create-infrastructure.sh
+++ scripts/create-infrastructure.sh
@@ -1,42 +1,70 @@
 #!/bin/bash
 
-image_id=ami-09693313102a30b2c
-instance_type=t2.micro
-vpc_id=vpc-04540e242cdfb35de
-key_name=user10
-security_group=...
-subnet_id=subnet-0d00d8b5d01e66e35
-shutdown_type=stop
-tags="ResourceType=instance,Tags=[{Key=installation_id,Value=user10-1},{Key=Name,Value=user10-vm1}]"
+USER_NAME=user10
+IMAGE_ID=ami-02fc24d56bc5f3d67 # ami-09693313102a30b2c
+INSTANCE_TYPE=t2.micro
+VPC_ID=vpc-04540e242cdfb35de
+KEY_NAME=user10
+SUBNET_ID=subnet-0d00d8b5d01e66e35
+SHUTDOWN_TYPE=stop
+TAGS="ResourceType=instance,Tags=[{Key=installation_id,Value=${USER_NAME}-1},{Key=Name,Value=NAME}]"
 
-start()
+start_vm()
 {
-  private_ip_address="10.2.1.101"
-  public_ip=associate-public-ip-address
+  local private_ip_address="$1"
+  local public_ip="$2"
+  local name="$3"
+
+  local tags=$(echo $TAGS | sed s/NAME/$name/)
+  # local tags=${TAGS/NAME/$name}
 
   aws ec2 run-instances \
-    --image-id "$image_id" \
-    --instance-type "$instance_type" \
-    --key-name "$key_name" \
-    --subnet-id "$subnet_id" \
-    --instance-initiated-shutdown-behavior "$shutdown_type" \
+    --image-id "$IMAGE_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --key-name "$KEY_NAME" \
+    --subnet-id "$SUBNET_ID" \
+    --instance-initiated-shutdown-behavior "$SHUTDOWN_TYPE" \
     --private-ip-address "$private_ip_address" \
     --tag-specifications "$tags" \
-    --${public_ip} 
+    --${public_ip}
 
   # --security-groups "$security_group" \
 
   #  [--block-device-mappings <value>]
   #  [--placement <value>]
   #  [--user-data <value>]
+}
 
+get_dns_name()
+{
+  local instance="$1"
+
+  aws ec2 describe-instances --instance-ids ${instance} \
+  | jq -r '.Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicDnsName'
+}
+
+start()
+{
+  start_log=$(
+    start_vm 10.2.1.101 associate-public-ip-address ${USER_NAME}-vm1
+  )
+
+  instance_id=$(echo "${start_log}" | jq -r .Instances[0].InstanceId)
+
+  # for i in {2..3}; do
+  #   start_vm 10.2.1.$((100+i)) no-associate-public-ip-address ${USER_NAME}-vm$i > /dev/null
+  # done
+
+  sleep 2
+  dns_name=$(get_dns_name "$instance_id")
+  echo $dns_name
 }
 
 stop()
 {
   ids=($(
     aws ec2 describe-instances \
-    --query 'Reservations[*].Instances[?KeyName==`'$key_name'`].InstanceId' \
+    --query 'Reservations[*].Instances[?KeyName==`'$KEY_NAME'`].InstanceId' \
     --output text
   ))
   aws ec2 terminate-instances --instance-ids "${ids[@]}"
